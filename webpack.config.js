a6a293625c5b4028b214e5201eb206c7037f8fe8
Kevin Qiu <kevinqiu96@gmail.com>
2017-11-25 15:48:10 -0800

    webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ var webpack = require('webpack');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: 'dist/assets',
+        path: './dist/assets',
         filename: 'bundle.js',
-        publicPath: 'assets'
+        publicPath: '/assets'
     },
     devServer: {
         inline: true,
